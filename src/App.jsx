bfc664bf98772affec774400e6dff54195544a67
Nizami Sevindi <nsevindi87@gmail.com>
2023-01-22 12:40:43 +0100

    millionare

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,39 @@
 import "./app.css"
+import { useState } from "react";
+import { moneyPyramid } from "./moneyPyramid";
 
 function App() {
+
+  const [questionNumber, setQuestionNumber] = useState(1)
+
+  
   return (
     <div className="app">
-    <div className="main">main</div>
-    <div className="pyramid">money</div>
+   {/*//!====MAIN SECTION======= */}
+    <div className="main">
+      <div className="top">
+        <div className="timer">30</div>
+
+      </div>
+      <div className="bottom">
+        question and answers
+      </div>
+    </div>
+
+
+
+    {/* //!====moneyPyramid======= */}
+    <div className="pyramid">
+      <ul className="moneyList">
+        {moneyPyramid.map((m)=>(
+          <li className={questionNumber === m.id ? "moneyListItem active": "moneyListItem"}>
+            <span className="moneyListItemNumber ">{m.id}</span>
+            <span className="moneyListItemAmount">{m.amount}</span>
+          </li>
+        ))}
+        
+      </ul>
+    </div>
     </div>
   );
 }
